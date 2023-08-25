40ff1e2f490b1c566432d1d97eb70ea331f2be4f
MonaChoudhary15 <choudhary15.mona.com>
2023-08-25 21:38:28 +0530

    banner changes

--- src/component/banner.js
+++ src/component/banner.js
@@ -2,16 +2,78 @@ import React from "react";
 import BannerImg from './images/banner.png';
 import OverlayImg from './images/Rectangle 4.png'
 
-const Banner=()=>{
-    return(
-        <div className="container-fluid banner">
-            <div className="row">
-            <div className="col p-0 overlay">
-            <img src={BannerImg} className="" alt="banner" />
-            <img src={OverlayImg} className="overlay-img" alt="banner" />
+const Banner = () => {
+    return (
+        <div>
+            <div className="container-fluid banner">
+                <div className="row">
 
-                </div>    
-            </div>        
+                    <div className="col-12 p-0 overlay position-relative">
+                        <div className="position-absolute top-50 start-50 translate-middle">
+                            <div className="row filter-first-row">
+                                <div className="col">
+                                    <button type="button" className="btn active-btn">Rent</button>
+                                </div>
+                                <div className="col">
+                                    <button type="button" className="btn">Buy</button>
+                                </div>
+                                <div className="col-md-9">
+                                    <div className="form-check">
+                                        <input className="form-check-input chk-box" type="checkbox" value="" id="flexCheckDefault" />
+                                        <label className="form-check-label" for="flexCheckDefault">
+                                            Show more search options
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <div className="input-group">
+                                        <input type="text" className="form-control" />
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <select className="form-select" aria-label="Default select example">
+                                        <option selected>Open this select menu</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                                <div className="col-md-2">
+                                    <select className="form-select" aria-label="Default select example">
+                                        <option selected>Open this select menu</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                                <div className="col-md-2">
+                                    <select className="form-select" aria-label="Default select example">
+                                        <option selected>Open</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                                <div className="col-md-1">
+                                    <button type="button" className="btn search-btn">&#xF52A;</button>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="float-end">Show more search options </div>
+                                </div>
+                            </div>
+                        </div>
+                        <img src={BannerImg} className="" alt="banner" />
+                        <img src={OverlayImg} className="overlay-img" alt="banner" />
+
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/component/innerContent.js
+++ src/component/innerContent.js
@@ -2,48 +2,59 @@ import React from "react";
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
+import Round1 from './images/brokerspot.png';
+import Round2 from './images/rachid.png';
+import Round3 from './images/neha.png';
+import Round4 from './images/aman.png';
+import Round5 from './images/john.png';
+import Round6 from './images/noha.png';
+import Round7 from './images/ankita.png';
+import Round8 from './images/roy.png';
+import Round9 from './images/noha1.png';
+
 
 const innerPage = () => {
     return (
-        <div className="container">
-            <div className="row">
+        <div className="container carousel-section">
+            <div className="row mt-2">
                 <div className="col-xs-12">
-                    <OwlCarousel className='owl-theme' loop margin={10} nav>
+                    <h2>Broker story</h2>
+                    <OwlCarousel className='owl-theme' loop items={9} nav>
                         <div class='item'>
-                            <h4>1</h4>
+                        <img src={Round1} className="" alt="banner" />
                         </div>
                         <div class='item'>
-                            <h4>2</h4>
+                        <img src={Round2} className="" alt="banner" />
+                                                </div>
+                        <div class='item'>
+                        <img src={Round3} className="" alt="banner" />
                         </div>
                         <div class='item'>
-                            <h4>3</h4>
+                        <img src={Round4} className="" alt="banner" />
                         </div>
                         <div class='item'>
-                            <h4>4</h4>
+                        <img src={Round5} className="" alt="banner" />
                         </div>
                         <div class='item'>
-                            <h4>5</h4>
+                        <img src={Round6} className="" alt="banner" />
                         </div>
                         <div class='item'>
-                            <h4>6</h4>
+                        <img src={Round7} className="" alt="banner" />
                         </div>
                         <div class='item'>
-                            <h4>7</h4>
+                        <img src={Round8} className="" alt="banner" />
                         </div>
                         <div class='item'>
-                            <h4>8</h4>
+                        <img src={Round9} className="" alt="banner" />
                         </div>
                         <div class='item'>
-                            <h4>9</h4>
+                        <img src={Round1} className="" alt="banner" />
                         </div>
                         <div class='item'>
-                            <h4>10</h4>
+                        <img src={Round2} className="" alt="banner" />
                         </div>
                         <div class='item'>
-                            <h4>11</h4>
-                        </div>
-                        <div class='item'>
-                            <h4>12</h4>
+                        <img src={Round3} className="" alt="banner" />
                         </div>
                     </OwlCarousel>
                 </div>
