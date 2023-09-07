24bef142de76237a0686d21128c13dcf17015d99
MonaChoudhary15 <choudhary15.mona.com>
2023-09-07 11:55:26 +0530

    mobile changes

--- src/component/banner.js
+++ src/component/banner.js
@@ -8,8 +8,8 @@ const Banner = () => {
 
     return (
         <div >
-            <div className="container-fluid banner">
-                <div className="row">
+            <div className="container-fluid banner ">
+                <div className="row d-none d-md-block">
 
                     <div className="col-12 p-0 overlay position-relative" id="filterId">
                         <div className="position-absolute top-50 start-50 translate-middle">
@@ -59,7 +59,7 @@ const Banner = () => {
                                     </select>
                                 </div>
                                 <div className="col">
-                                    <select className="form-select" aria-label="Default select example" style={{minWidth: '100px'}}>
+                                    <select className="form-select" aria-label="Default select example" style={{ minWidth: '100px' }}>
                                         <option selected>Price</option>
                                         <option value="1">One</option>
                                         <option value="2">Two</option>
@@ -85,8 +85,84 @@ const Banner = () => {
                         <img src={OverlayImg} className="overlay-img" alt="banner" />
 
                     </div>
-
                 </div>
+
+                <div className="row d-block d-md-none sm-banner">
+                    <div className="col px-0">
+                    <img src={BannerImg} className="" alt="banner" />
+                    </div>
+                    <div className="col-xs-12">
+                        <div className="d-grid gap-1 d-flex justify-content-center buy-sell my-3">
+                            <button className="btn active-btn" type="button">Buy</button>
+                            <button className="btn" type="button">Rent</button>
+                        </div>
+                    </div>
+
+                    <div className="row m-auto">
+                        <div className="col-md-5 mb-2">
+                            <Multiselect
+                                // options={'xyz'}
+                                // selectedValues={'xyz'} 
+                                // onSelect={this.onSelect} 
+                                // onRemove={this.onRemove} 
+                                displayValue="name"
+                                className=""
+                            />
+                        </div>
+                        <div className="col-md-2 mb-2">
+                            <select className="form-select" aria-label="Default select example">
+                                <option selected>Property Type</option>
+                                <option value="Apartment">Apartment</option>
+                                <option value="Villa">Villa</option>
+                                <option value="Townhouse">Townhouse</option>
+                                <option value="Penthouse">Penthouse</option>
+                                <option value="Compound">Compound</option>
+                                <option value="Duplex">Duplex</option>
+                                <option value="Full Floor">Full Floor</option>
+                                <option value="Half Floor">Half Floor</option>
+                                <option value="Whole Building">Whole Building</option>
+                                <option value="Bulk Rent Unit">Bulk Rent Unit</option>
+                                <option value="Bungalow">Bungalow</option>
+                                <option value="Hotel & Hotel Apartment">Hotel & Hotel Apartment</option>
+                            </select>
+
+                        </div>
+                        <div className="col-md-2 mb-2">
+                            <select className="form-select" aria-label="Default select example">
+                                <option selected>Beds &amp; Baths</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div className="col mb-2">
+                            <select className="form-select" aria-label="Default select example" style={{ minWidth: '100px' }}>
+                                <option selected>Price</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div className="col mb-2">
+                            <button type="button" className="btn search-btn">
+                                <img src={SearchImg} className="" alt="banner" />
+                            </button>
+                        </div>
+                        <div className="col-md-12 my-1">
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                <label className="form-check-label" for="flexCheckDefault">
+                                    Show more search options
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
             </div>
 
         </div>
--- src/component/nav.js
+++ src/component/nav.js
@@ -3,43 +3,66 @@ import Logo from './images/logo.png';
 
 
 const Nav = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container main-nav">
-                <a className="navbar-brand" href="#"><img src={Logo} className="" alt="logo" /></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-                    <div className="d-flex ms-auto">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">About Us</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Contact Us</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Announcements</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Meeting</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Account</a>
-                            </li>
-                            <li>
-                                <button className="btn btn-outline-success" >Log in</button>
-                            </li>
-                        </ul>
+    const removeClass = () => {
+
+    }
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light px-2">
+                <div className="container main-nav">
+                    <a className="navbar-brand" href="#"><img src={Logo} className="" alt="logo" /></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+                        <div className="d-flex ms-auto">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="#">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">About Us</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Contact Us</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Announcements</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Meeting</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Account</a>
+                                </li>
+                                <li>
+                                    <button className="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#exampleModal">Log in</button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div class="modal fade login-type-modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Modal body text goes here.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </nav>
+        </>
     )
 }
 export default Nav
