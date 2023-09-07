0b7aa90f31f6573d386b65d9614c769e59b57ff4
Abhinav Rana <48202760+ranaabhinav726@users.noreply.github.com>
2023-09-07 13:58:12 +0530

    api

--- src/utils/apifactory.js
+++ src/utils/apifactory.js
@@ -3,8 +3,9 @@ const BASE_URL = 'https://acc.brokkerspot.com/api/'
 
 
 const APIS = {
-    GET_BANNER_API: BASE_URL + 'slider',
-    
+    GET_SLIDER_API: BASE_URL + 'slider',
+    GET_BANNER_API:BASE_URL+'banners'
+
 
 }
 export {
